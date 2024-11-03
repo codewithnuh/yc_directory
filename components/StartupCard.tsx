65c13bb162114b00421a4fe89382590c4f8f23fe
codewithnuh <nuh25792@gmail.com>
2024-11-03 19:25:22 +0500

    Style: adds styles to startup card component

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,6 +2,8 @@ import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
@@ -11,6 +13,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
     title,
     _id,
     image,
+    description,
+    category,
   } = post;
   return (
     <li className="startup-card group">
@@ -26,7 +30,31 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
           <Link href={`/user/${authorId}`}>
             <p className="text-16-medium line-clamp-1">{name}</p>
           </Link>
+          <Link href={`/startup/${_id}`}>
+            <h2 className="text-26-semibold line-clamp-1">{title}</h2>
+          </Link>
         </div>
+        <Link href={`/user/${authorId}`}>
+          <div className="rounded-full bg-black w-8 h-8"></div>
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <Image
+          src={image}
+          width={250}
+          height={250}
+          className="startup-card_img"
+          alt="IMG"
+        />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
       </div>
     </li>
   );
