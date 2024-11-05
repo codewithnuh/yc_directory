4f988a23116a57003a8ccf2bd1313bbc7694be1f
codewithnuh <nuh25792@gmail.com>
2024-11-05 18:02:59 +0500

    refactor: startup details page main img adjusted properly

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -3,6 +3,7 @@ import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { notFound } from "next/navigation";
 import React from "react";
+import Image from "next/image";
 export const experimental_ppr = true;
 const Startup = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -14,6 +15,15 @@ const Startup = async ({ params }: { params: Promise<{ id: string }> }) => {
         <p className="tag">{formatDate(post?._createdAt)}</p>
         <h1 className="heading">{post?.title}</h1>
         <p className="sub-heading !max-w-5xl">{post?.description}</p>
+      </section>
+      <section className="section_container">
+        <Image
+          src={post.image}
+          alt="thumbnail"
+          width={1200}
+          height={600}
+          className="w-full rounded-xl h-80 object-cover"
+        />
       </section>
     </>
   );
